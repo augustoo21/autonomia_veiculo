a3bf152a3c43a7a0630e26acdeb8e2b15d103d80
augustoo21 <augustoo@edu.univali.br>
2019-10-21 20:37:33 -0300

    mudança na tela inicial, acrescentei label de exibição de autonomia

--- app/src/main/java/com/magnani/dispositivosmoveis/autonomia_veiculo/MainActivity.java
+++ app/src/main/java/com/magnani/dispositivosmoveis/autonomia_veiculo/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
+        Button button = findViewById(R.id.btn_abastecimento);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
